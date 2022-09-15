e7af5576aa8965f60ff328510ae1deca9021b5f6
Leandro Santos Ribeiro Lacerda <leandrolacerda.sistema@gmail.com>
2022-09-15 20:09:35 -0300

    Implementação ClienteRepository

--- src/main/java/com/algawork/algalog/controller/ClientesController.java
+++ src/main/java/com/algawork/algalog/controller/ClientesController.java
@@ -2,23 +2,40 @@ package com.algawork.algalog.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algawork.algalog.domain.model.Cliente;
 import com.algawork.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClientesController {
 
-	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	/*
+	 * @Autowired 
+	 * private ClienteRepository clienteRepository;
+	 */
+	
+	
+	/* CASO NÃO QUEIRA UTLIZAR A INJEÇÃO DE INSTANCIA GERENCIADA PELO SPRING
+	 * private ClienteRepository clienteRepository;
+	 * 
+	 * public ClientesController(ClienteRepository clienteRepository) { super();
+	 * this.clienteRepository = clienteRepository; }
+	 */
+
+
+
 	@GetMapping("clientes")
 	public List<Cliente> listar() {
 
-		return clienteRepository.findAll();
+		//return clienteRepository.findByNome("Leandro Lacerda"); 
+		return clienteRepository.findByNomeContaining("a");
 	}
 
 }
